e376b7ab59c431f3cf9161a4357cd0f35317df3c
Sergey Fomenko <fomenko.personal@gmail.com>
2017-08-29 17:39:29 +0300

    New version

--- task-1/js/scripts.js
+++ task-1/js/scripts.js
@@ -1,20 +1,37 @@
-var distance = +prompt('Укажите расстояние в километрах');
+// var distance = +prompt('Укажите расстояние в километрах');
+//
+// function pek() {
+//   var firstPrice = (distance * 0.9);
+//   alert('Компания ПЭК ' + firstPrice + ' руб');
+// }
+//
+// function dl() {
+//   var secondPrice = (distance * 1);
+//   alert('Компания Деловые Линии ' + secondPrice + ' руб');
+// }
+//
+// function best() {
+//   var thirdPrice = (distance * 2);
+//   alert('Компания Лучшая компания ' + thirdPrice + ' руб');
+// }
+//
+// pek();
+// dl();
+// best();
 
-function pek() {
-  var firstPrice = (distance * 0.9);
-  alert('Компания ПЭК ' + firstPrice + ' руб');
+'use strict';
+
+function calcDistance(distance) {
+    var companyList = {
+    "pek": 0.9,
+    "business": 1,
+    "best": 2
+  };
+    for (var company in companyList) {
+        companyList[company] = companyList[company] * distance + " " + "rub";
+    }
+
+    console.log(companyList);
 }
 
-function dl() {
-  var secondPrice = (distance * 1);
-  alert('Компания Деловые Линии ' + secondPrice + ' руб');
-}
-
-function best() {
-  var thirdPrice = (distance * 2);
-  alert('Компания Лучшая компания ' + thirdPrice + ' руб');
-}
-
-pek();
-dl();
-best();
+calcDistance(10);
