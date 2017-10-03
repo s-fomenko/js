771ad18d04d67f0255c26f957f2913ff646fe93b
Sergey Fomenko <fomenko.personal@gmail.com>
2017-10-03 12:31:00 +0300

    update task-1

--- task-1/js/scripts.js
+++ task-1/js/scripts.js
@@ -1,24 +1,3 @@
-// var distance = +prompt('Укажите расстояние в километрах');
-//
-// function pek() {
-//   var firstPrice = (distance * 0.9);
-//   alert('Компания ПЭК ' + firstPrice + ' руб');
-// }
-//
-// function dl() {
-//   var secondPrice = (distance * 1);
-//   alert('Компания Деловые Линии ' + secondPrice + ' руб');
-// }
-//
-// function best() {
-//   var thirdPrice = (distance * 2);
-//   alert('Компания Лучшая компания ' + thirdPrice + ' руб');
-// }
-//
-// pek();
-// dl();
-// best();
-
 'use strict';
 
 function calcDistance(distance) {
