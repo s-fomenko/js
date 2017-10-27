5fa9b487a266681b2d29e3361e393b2b64622a51
Sergey Fomenko <s.fomenko@andersenlab.com>
2017-10-27 13:54:19 +0300

    Task 4

---
+++ js/4.js
@@ -0,0 +1,24 @@
+var distanceRange = [10, 15, 20];
+var commonPriceResult = [];
+
+var companyList = {
+  "pek": 0.9,
+  "business": 1,
+  "best": 2
+};
+
+function addCompany(companyName, companyFactor) {
+  companyList[companyName] = companyFactor;
+}
+
+addCompany('newCompany', 50);
+
+for (var company in companyList) {
+  distanceRange.forEach(function (distance) {
+    var priceResult = {};
+    priceResult[company] = companyList[company] * distance + " " + "rub";
+    commonPriceResult.push(priceResult);
+  });
+}
+
+console.log(commonPriceResult);
